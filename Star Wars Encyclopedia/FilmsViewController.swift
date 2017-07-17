551a659c06016a22e7bb9212c48713776a0bd903
sbishop7 <seth.bishop@gmail.com>
2017-07-17 16:12:35 -0700

    Convert to MVC

--- Star Wars Encyclopedia/FilmsViewController.swift
+++ Star Wars Encyclopedia/FilmsViewController.swift
@@ -11,43 +11,15 @@ import UIKit
 class FilmsViewController: UITableViewController {
 
     var films = [String]()
-    var url = URL(string: "http://swapi.co/api/films/")
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // specify the url that we will be sending the GET request to
-        //        var url = URL(string: "http://swapi.co/api/people/?page=8")
-        // create a URLSession to handle the request tasks
-        //        var next = true
-        getApiData(url: self.url!)
-        
-        
-    }
-    
-    func getApiData(url: URL) {
-        let session = URLSession.shared
-        // create a "data task" to make the request and run completion handler
-        let task = session.dataTask(with: url, completionHandler: {
-            // see: Swift closure expression syntax
+        StarWarsModel.getAllFilms(completionHandler: {
             data, response, error in
-            // data -> JSON data, response -> headers and other meta-information, error-> if one occurred
-            // "do-try-catch" blocks execute a try statement and then use the catch statement for errors
-            do {
-                
-                // try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
-                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
-                    DispatchQueue.main.async {
-                        if (jsonResult["next"] as? NSNull) != nil {
-                            print ("it's null")
-                        }
-                        if let newURL = jsonResult["next"] as? String {
-                            self.url = URL(string: newURL )
-                            print (self.url!)
-                            
-                            self.getApiData(url: URL(string: newURL )!)
-                        }
-                        
+                do {
+                    // try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
+                    if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
                         if let results = jsonResult["results"] {
                             
                             // coercing the results object as an NSArray and then storing that in resultsArray
@@ -60,21 +32,70 @@ class FilmsViewController: UITableViewController {
                                 self.films.append(filmDict["title"]! as! String)
                             }
                         }
-                        
-                        self.tableView.reloadData()
+                        DispatchQueue.main.async {
+                            self.tableView.reloadData()
+                        }
                     }
                     
+                } catch {
+                    print(error)
                 }
-            } catch {
-                print(error)
-            }
+
         })
         
-        // execute the task and then wait for the response
-        // to run the completion handler. This is async!
         
-        task.resume()
     }
+    
+//    func getApiData(url: URL) {
+//        let session = URLSession.shared
+//        // create a "data task" to make the request and run completion handler
+//        let task = session.dataTask(with: url, completionHandler: {
+//            // see: Swift closure expression syntax
+//            data, response, error in
+//            // data -> JSON data, response -> headers and other meta-information, error-> if one occurred
+//            // "do-try-catch" blocks execute a try statement and then use the catch statement for errors
+//            do {
+//                
+//                // try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
+//                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+//                    DispatchQueue.main.async {
+//                        if (jsonResult["next"] as? NSNull) != nil {
+//                            print ("it's null")
+//                        }
+//                        if let newURL = jsonResult["next"] as? String {
+//                            self.url = URL(string: newURL )
+//                            print (self.url!)
+//                            
+//                            self.getApiData(url: URL(string: newURL )!)
+//                        }
+//                        
+//                        if let results = jsonResult["results"] {
+//                            
+//                            // coercing the results object as an NSArray and then storing that in resultsArray
+//                            let resultsArray = results as! NSArray
+//                            // now we can run NSArray methods like count and firstObject
+//                            //
+//                            for film in resultsArray {
+//                                let filmDict = film as! NSDictionary
+//                                print(filmDict["title"]!)
+//                                self.films.append(filmDict["title"]! as! String)
+//                            }
+//                        }
+//                        
+//                        self.tableView.reloadData()
+//                    }
+//                    
+//                }
+//            } catch {
+//                print(error)
+//            }
+//        })
+//        
+//        // execute the task and then wait for the response
+//        // to run the completion handler. This is async!
+//        
+//        task.resume()
+//    }
     
     
     
--- Star Wars Encyclopedia/PeopleViewController.swift
+++ Star Wars Encyclopedia/PeopleViewController.swift
@@ -16,52 +16,36 @@ class PeopleViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // specify the url that we will be sending the GET request to
-//        var url = URL(string: "http://swapi.co/api/people/?page=8")
-        // create a URLSession to handle the request tasks
-//        var next = true
-        getApiData(url: self.url!)
-        
-
+        getApiData(thisUrl: url!)
     }
     
-    func getApiData(url: URL) {
-        let session = URLSession.shared
-        // create a "data task" to make the request and run completion handler
-        let task = session.dataTask(with: url, completionHandler: {
-            // see: Swift closure expression syntax
+    func getApiData(thisUrl: URL) {
+        StarWarsModel.getAllPeople(url: thisUrl, completionHandler: {
             data, response, error in
-            // data -> JSON data, response -> headers and other meta-information, error-> if one occurred
-            // "do-try-catch" blocks execute a try statement and then use the catch statement for errors
             do {
-                
                 // try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
                 if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+                    if (jsonResult["next"] as? NSNull) != nil {
+                        print ("it's null")
+                    }
+                    if let newURL = jsonResult["next"] as? String {
+                        self.url = URL(string: newURL )
+                        print (self.url!)
+                        self.getApiData(thisUrl: URL(string: newURL )!)
+                    }
+                    if let results = jsonResult["results"] {
+                        // coercing the results object as an NSArray and then storing that in resultsArray
+                        let resultsArray = results as! NSArray
+                        // now we can run NSArray methods like count and firstObject
+                        //
+                        for person in resultsArray {
+                            let personDict = person as! NSDictionary
+                            print(personDict["name"]!)
+                            self.people.append(personDict["name"]! as! String)
+                        }
+                    }
                     DispatchQueue.main.async {
-                        if (jsonResult["next"] as? NSNull) != nil {
-                            print ("it's null")
-                        }
-                        if let newURL = jsonResult["next"] as? String {
-                            self.url = URL(string: newURL )
-                            print (self.url!)
-                            
-                            self.getApiData(url: URL(string: newURL )!)
-                        }
-
-                        if let results = jsonResult["results"] {
-                        
-                            // coercing the results object as an NSArray and then storing that in resultsArray
-                            let resultsArray = results as! NSArray
-                            // now we can run NSArray methods like count and firstObject
-                            //
-                            for person in resultsArray {
-                                let personDict = person as! NSDictionary
-                                print(personDict["name"]!)
-                                self.people.append(personDict["name"]! as! String)
-                            }
-                        }
-                        
-                    self.tableView.reloadData()
+                        self.tableView.reloadData()
                     }
                     
                 }
@@ -69,12 +53,59 @@ class PeopleViewController: UITableViewController {
                 print(error)
             }
         })
-        
-        // execute the task and then wait for the response
-        // to run the completion handler. This is async!
-        
-        task.resume()
+
     }
+    
+//    func getApiData(url: URL) {
+//        let session = URLSession.shared
+//        // create a "data task" to make the request and run completion handler
+//        let task = session.dataTask(with: url, completionHandler: {
+//            // see: Swift closure expression syntax
+//            data, response, error in
+//            // data -> JSON data, response -> headers and other meta-information, error-> if one occurred
+//            // "do-try-catch" blocks execute a try statement and then use the catch statement for errors
+//            do {
+//                
+//                // try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
+//                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+////                    DispatchQueue.main.async {
+//                        if (jsonResult["next"] as? NSNull) != nil {
+//                            print ("it's null")
+//                        }
+//                        if let newURL = jsonResult["next"] as? String {
+//                            self.url = URL(string: newURL )
+//                            print (self.url!)
+//                            
+//                            self.getApiData(url: URL(string: newURL )!)
+//                        }
+//
+//                        if let results = jsonResult["results"] {
+//                        
+//                            // coercing the results object as an NSArray and then storing that in resultsArray
+//                            let resultsArray = results as! NSArray
+//                            // now we can run NSArray methods like count and firstObject
+//                            //
+//                            for person in resultsArray {
+//                                let personDict = person as! NSDictionary
+//                                print(personDict["name"]!)
+//                                self.people.append(personDict["name"]! as! String)
+//                            }
+//                        }
+//                    DispatchQueue.main.async {
+//                        self.tableView.reloadData()
+//                    }
+//                    
+//                }
+//            } catch {
+//                print(error)
+//            }
+//        })
+//        
+//        // execute the task and then wait for the response
+//        // to run the completion handler. This is async!
+//        
+//        task.resume()
+//    }
 
     
     
